aa067fb508172071506f2fd72933473ce9dfc170
jeeltpatel <patel.jeel@northeaster.edu>
2022-04-16 12:37:29 -0400

    :package: helment,cors and morgan added

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin:'http://localhost:3000'
+}));
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:'Hello World',
+    })
+})
+
+app.use((req,res,next)=>{
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+});
 
 const port = process.env.PORT || 1337;
 app.listen(port,()=>{
