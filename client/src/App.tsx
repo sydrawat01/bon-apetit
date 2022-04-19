5dc987cd7576cd06c2c641852b45636152b5c852
uk1997 <kolhe.u@northeastern.edu>
2022-04-19 13:55:51 -0400

    Implement react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,26 @@
 import { FC } from 'react'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './styles/layout/layout.scss'
-
+import Landing from './components/Pages/Landing'
+import HomePage from './components/Pages/HomePage'
+import ForgotPassword from './components/Pages/ForgotPassword'
+import PageNotFound from './components/Pages/PageNotFound'
 import Spinner from './components/UI/Spinner'
 
 const App: FC = () => {
   return (
-    <>
-      <h1>{process.env.APP_TITLE}</h1>
-      <Spinner />
-    </>
+    <BrowserRouter>
+      <>
+        <h1>{process.env.APP_TITLE}</h1>
+        <Spinner />
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/landing" component={Landing} />
+          <Route path="/forgotpassword" component={ForgotPassword} />
+          <Route path="/404" component={PageNotFound} />
+        </Switch>
+      </>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+const HomePage: FC = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Hello</p>
+    </div>
+  )
+}
+
+export default HomePage
