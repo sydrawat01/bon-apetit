212b239364ebb970db02c5a2960254cd1f993de3
uk1997 <kolhe.u@northeastern.edu>
2022-04-19 15:43:11 -0400

    Making requested changes

--- client/src/components/Pages/Landing.tsx
+++ client/src/components/Pages/Landing.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 
 const Landing: FC = () => {
   return <div>Landing</div>
--- client/src/components/Pages/PageNotFound.tsx
+++ client/src/components/Pages/PageNotFound.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 
 const PageNotFound: FC = () => {
   return <div>PageNotFound</div>
