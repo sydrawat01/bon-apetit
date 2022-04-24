58d73de363126cb301141b42f7fafd4413d14de9
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-04-24 15:16:39 -0400

    :round_pushpin: Drop pin location funnctionality added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Landing from './components/Pages/Landing/Landing'
 import HomePage from './components/Pages/Home/HomePage'
@@ -11,12 +11,12 @@ import './styles/layout/layout.scss'
 const App: FC = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" component={HomePage} exact />
-        <Route path="/landing" component={Landing} />
-        <Route path="/forgotpassword" component={ForgotPassword} />
-        <Route path="/404" component={PageNotFound} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/404" element={<PageNotFound />} />
+      </Routes>
     </BrowserRouter>
   )
 }
---
+++ client/src/models/LogEntry.ts
@@ -0,0 +1,14 @@
+import { UserDoc } from './User'
+
+export type LogEntry = {
+  title: string
+  description?: string
+  comments: string
+  rating: number
+  latitude: number
+  longitude: number
+  visitDate: string
+  user?: UserDoc
+}
+
+export type LogEntryDoc = LogEntry & { _id: string }
