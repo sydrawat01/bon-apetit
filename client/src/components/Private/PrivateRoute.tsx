ea6d7681f50c1f47987c1b64bd97f6afebfdcd72
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-04-25 23:19:11 -0400

    :unlock: add private route for map home

---
+++ client/src/components/Private/PrivateRoute.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from 'react'
+import { useQuery } from 'react-query'
+import { Navigate } from 'react-router-dom'
+import Spinner from '../UI/Spinner'
+import { loadUser } from '../../services/users'
+
+type PrivateRouteType = {
+  children?: ReactNode
+}
+
+const PrivateRoute: FC<PrivateRouteType> = ({ children }) => {
+  const { data, isLoading } = useQuery('me', loadUser)
+
+  if (isLoading) return <Spinner />
+
+  return data && data.user ? <>{children}</> : <Navigate to="/login" />
+}
+
+export default PrivateRoute
