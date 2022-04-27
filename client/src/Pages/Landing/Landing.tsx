310c41ad6956f4ae6e95a079c827c852b0668d23
Shubhangi-Srivastava <srivastava.sh@northeastern.edu>
2022-04-26 22:28:48 -0400

    Resolves issue #30, added more styles and imported variables.scss to incorporate and use existing variables from the latter

--- client/src/Pages/Landing/Landing.tsx
+++ client/src/Pages/Landing/Landing.tsx
@@ -17,7 +17,10 @@ const Landing: FC = () => {
 
   return (
     <div className="container public-logs-container">
-      <h1>Browse Logs</h1>
+      <h1 className="head-title">Browse Logs</h1>
+      <p className="brief">
+        Explore curated lists of reviews of top places in Boston
+      </p>
       <section className="logs-list">
         {data && data.length > 0 ? (
           data.map((entry) => (
